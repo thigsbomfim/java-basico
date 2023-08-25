27f6ee118d9b4232ad7c8cd4469997bacad1f7d8
thigsbomfim <thigs.bomfim@gmail.com>
2023-08-24 22:48:30 -0300

    feat: adding directory flux control

---
+++ controle-de-fluxos/src/ResultadoEscolar3.java
@@ -0,0 +1,23 @@
+// Condição ternária
+
+/* 
+
+// Cenário 1
+public class ResultadoEscolar3 {
+	public static void main(String[] args) {
+		int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
+
+*/
+
+// Cenário 2
+public class ResultadoEscolar3 {
+	public static void main(String[] args) {
+		int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
---
+++ controle-de-fluxos/src/SistemaMedida.java
@@ -0,0 +1,49 @@
+// SistemaMedida.java
+
+// Switch - Case
+
+/* 
+
+  // Modo condicional if/else
+  public class SistemaMedida {
+    public static void main(String[] args) {
+      String sigla = "M";
+
+      if(sigla == "P")
+        System.out.println("PEQUENO");
+      else if(sigla == "M")
+        System.out.println("MÉDIO");
+      else if(sigla == "G")
+        System.out.println("GRANDE");
+      else
+        System.out.println("INDEFINIDO");
+    }
+  }
+
+*/
+
+// SistemaMedida.java
+
+// Modo condicional switch / case
+public class SistemaMedida {
+	public static void main(String[] args) {
+		String sigla = "M";
+
+		switch ( sigla ) {
+		case "P":{
+			System.out.println("PEQUENO");
+			break;
+		}
+		case "M":{
+			System.out.println("MÉDIO");
+			break;
+		}
+		case "G":{
+			System.out.println("GRANDE");
+			break;
+		}
+		default:
+			System.out.println("INDEFINIDO");
+		}
+	}
+}
